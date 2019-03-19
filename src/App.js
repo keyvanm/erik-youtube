b00dd3d793ba7d48ab7644c2b0da34fe12216e3a
Keyvan Mosharraf <keyvan@keyvanm.com>
2019-03-19 16:26:41 -0700

    YouTube embed done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import { Container, Grid } from 'semantic-ui-react';
 import SiteHeader from './components/SiteHeader';
+import ResponsiveEmbed from 'react-responsive-embed';
+
 
 class App extends Component {
   render() {
@@ -11,10 +13,10 @@ class App extends Component {
         <Container>
           <Grid>
             <Grid.Column width={11}>
-            
+              <ResponsiveEmbed src='https://www.youtube.com/embed/2yqz9zgoC-U' allowFullScreen />
             </Grid.Column>
             <Grid.Column width={5}>
-            
+              
             </Grid.Column>
           </Grid>
         </Container>
