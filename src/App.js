b443912a7aa9e73c03f98b36295cac5b3b782ce2
Keyvan Mosharraf <keyvan@keyvanm.com>
2019-03-19 16:37:59 -0700

    Mock up for youtube suggested videos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Item, Image } from 'semantic-ui-react';
 import SiteHeader from './components/SiteHeader';
 import ResponsiveEmbed from 'react-responsive-embed';
+import _ from 'lodash';
 
 
 class App extends Component {
@@ -16,7 +17,23 @@ class App extends Component {
               <ResponsiveEmbed src='https://www.youtube.com/embed/2yqz9zgoC-U' allowFullScreen />
             </Grid.Column>
             <Grid.Column width={5}>
-              
+            <Item.Group>
+              {
+                _.times(10, () => (
+                  <Item>
+                    <Item.Image src='https://via.placeholder.com/168x94.png' />
+                    <Item.Content>
+                      <Item.Header as='a'>Header</Item.Header>
+                      <Item.Meta>Description</Item.Meta>
+                      <Item.Description>
+                        <Image src='/images/wireframe/short-paragraph.png' />
+                      </Item.Description>
+                      <Item.Extra>Additional Details</Item.Extra>
+                    </Item.Content>
+                  </Item>
+                ))
+              }
+            </Item.Group>
             </Grid.Column>
           </Grid>
         </Container>
