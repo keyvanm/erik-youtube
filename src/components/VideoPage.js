555bad136a039b5a1d708779deee352d74bf7a1e
Keyvan Mosharraf <keyvan@keyvanm.com>
2019-03-19 18:02:39 -0700

    Added secondary menu to video page

--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Item } from 'semantic-ui-react';
+import { Grid, Item, Header, Menu } from 'semantic-ui-react';
 import ResponsiveEmbed from 'react-responsive-embed';
 import _ from 'lodash';
 
@@ -9,6 +9,31 @@ const VideoPage = ({match}) => {
     <Grid>
       <Grid.Column width={11}>
         <ResponsiveEmbed src={`https://www.youtube.com/embed/${match.params.videoId}`} allowFullScreen />
+        <Header content="VideoTitle" subheader="Views"/>
+        <Menu pointing secondary>
+          <Menu.Menu position='right'>
+            <Menu.Item
+              icon="thumbs up"
+              name="Likes"
+            />
+            <Menu.Item
+              icon="thumbs down"
+              name="Dislikes"
+            />
+            <Menu.Item
+              icon="share"
+              name="Share"
+            />
+            <Menu.Item
+              icon="save"
+              name="Save"
+            />
+            <Menu.Item
+              icon="ellipsis horizontal"
+            />
+          </Menu.Menu>
+          
+        </Menu>
       </Grid.Column>
       <Grid.Column width={5}>
         <Item.Group>
