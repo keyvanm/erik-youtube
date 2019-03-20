ebcd875ade8b9fbb912e303aba83ec42930ee3c4
Keyvan Mosharraf <keyvan@keyvanm.com>
2019-03-19 17:41:31 -0700

    Added dynamic embed

--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -4,27 +4,29 @@ import ResponsiveEmbed from 'react-responsive-embed';
 import _ from 'lodash';
 
 
-const VideoPage = () => (
-  <Grid>
-    <Grid.Column width={11}>
-      <ResponsiveEmbed src='https://www.youtube.com/embed/2yqz9zgoC-U' allowFullScreen />
-    </Grid.Column>
-    <Grid.Column width={5}>
-      <Item.Group>
-        {
-          _.times(10, () => (
-            <Item>
-              <Item.Image src='https://via.placeholder.com/168x94.png' />
-              <Item.Content>
-                <Item.Header as='a'>Video Title</Item.Header>
-                <Item.Meta>Video Channel</Item.Meta>
-              </Item.Content>
-            </Item>
-          ))
-        }
-      </Item.Group>
-    </Grid.Column>
-  </Grid>
-)
+const VideoPage = ({match}) => {
+  return (
+    <Grid>
+      <Grid.Column width={11}>
+        <ResponsiveEmbed src={`https://www.youtube.com/embed/${match.params.videoId}`} allowFullScreen />
+      </Grid.Column>
+      <Grid.Column width={5}>
+        <Item.Group>
+          {
+            _.times(10, () => (
+              <Item>
+                <Item.Image src='https://via.placeholder.com/168x94.png' />
+                <Item.Content>
+                  <Item.Header as='a'>Video Title</Item.Header>
+                  <Item.Meta>Video Channel</Item.Meta>
+                </Item.Content>
+              </Item>
+            ))
+          }
+        </Item.Group>
+      </Grid.Column>
+    </Grid>
+  )
+}
 
 export default VideoPage
