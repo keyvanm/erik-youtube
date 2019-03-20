92f75563ee47c95d7ccef0e8dd5c5ecbb640296b
Keyvan Mosharraf <keyvan@keyvanm.com>
2019-03-19 17:21:53 -0700

    Refactor Video Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import { Container, Grid, Item, Image } from 'semantic-ui-react';
-import ResponsiveEmbed from 'react-responsive-embed';
+import { Container } from 'semantic-ui-react';
 import _ from 'lodash';
-
 import './App.css';
-
 import SiteHeader from './components/SiteHeader';
-
-
-
+import VideoPage from './components/VideoPage';
 
 class App extends Component {
   render() {
@@ -16,26 +11,7 @@ class App extends Component {
       <div>
         <SiteHeader />
         <Container className="main-container">
-          <Grid>
-            <Grid.Column width={11}>
-              <ResponsiveEmbed src='https://www.youtube.com/embed/2yqz9zgoC-U' allowFullScreen />
-            </Grid.Column>
-            <Grid.Column width={5}>
-              <Item.Group>
-                {
-                  _.times(10, () => (
-                    <Item>
-                      <Item.Image src='https://via.placeholder.com/168x94.png' />
-                      <Item.Content>
-                        <Item.Header as='a'>Video Title</Item.Header>
-                        <Item.Meta>Video Channel</Item.Meta>
-                      </Item.Content>
-                    </Item>
-                  ))
-                }
-              </Item.Group>
-            </Grid.Column>
-          </Grid>
+          <VideoPage />
         </Container>
       </div>
     );
---
+++ src/components/VideoPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Grid, Item } from 'semantic-ui-react';
+import ResponsiveEmbed from 'react-responsive-embed';
+import _ from 'lodash';
+
+
+const VideoPage = () => (
+  <Grid>
+    <Grid.Column width={11}>
+      <ResponsiveEmbed src='https://www.youtube.com/embed/2yqz9zgoC-U' allowFullScreen />
+    </Grid.Column>
+    <Grid.Column width={5}>
+      <Item.Group>
+        {
+          _.times(10, () => (
+            <Item>
+              <Item.Image src='https://via.placeholder.com/168x94.png' />
+              <Item.Content>
+                <Item.Header as='a'>Video Title</Item.Header>
+                <Item.Meta>Video Channel</Item.Meta>
+              </Item.Content>
+            </Item>
+          ))
+        }
+      </Item.Group>
+    </Grid.Column>
+  </Grid>
+)
+
+export default VideoPage
