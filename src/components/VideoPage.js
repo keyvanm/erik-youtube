1afd92c0b4b5376f13d1c0cc4e53b1a8276a91f3
Keyvan Mosharraf <keyvan@keyvanm.com>
2019-03-19 18:18:45 -0700

    Built the stuff under video

--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Grid, Item, Header, Menu } from 'semantic-ui-react';
+import { Grid, Item, Header, Menu, Image, Button, Container, Divider, Feed } from 'semantic-ui-react';
 import ResponsiveEmbed from 'react-responsive-embed';
 import _ from 'lodash';
 
+
+const image = 'https://via.placeholder.com/150'
+const date = '3 days ago'
+const summary = 'Laura Faucet'
+const extraText = "Have you seen what's going on in Israel? Can you believe it."
 
 const VideoPage = ({match}) => {
   return (
     <Grid>
       <Grid.Column width={11}>
         <ResponsiveEmbed src={`https://www.youtube.com/embed/${match.params.videoId}`} allowFullScreen />
-        <Header content="VideoTitle" subheader="Views"/>
+        <Header as='h1' content="VideoTitle" subheader="Views"/>
         <Menu pointing secondary>
           <Menu.Menu position='right'>
             <Menu.Item
@@ -32,8 +37,26 @@ const VideoPage = ({match}) => {
               icon="ellipsis horizontal"
             />
           </Menu.Menu>
-          
         </Menu>
+        <Header as='h3'>
+          <Image circular src='https://via.placeholder.com/150' /> 
+          <Header.Content>
+            UserName
+            <Header.Subheader>Published on Date</Header.Subheader>
+          </Header.Content>
+          <Button primary style={{ float: 'right' }}>Subscribe SubCount</Button>
+        </Header>
+        <Container text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus egestas ligula, at porttitor arcu rutrum feugiat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec a metus finibus, finibus nibh at, ullamcorper sapien. Etiam volutpat lacus quis tellus tempus vulputate. Integer lobortis aliquet mi, sed lobortis nibh hendrerit vel. Sed in luctus ligula, sit amet commodo massa. Nullam leo orci, vestibulum id urna eu, vestibulum tincidunt eros. Mauris dignissim at dui vitae efficitur. Maecenas commodo lacinia nibh. In hac habitasse platea dictumst. Vestibulum ornare sollicitudin mi, non ullamcorper orci. Maecenas non quam id libero sollicitudin malesuada non feugiat leo.
+        </Container>
+        <Divider />
+        <Feed>
+          {
+            _.times(10, () => (
+              <Feed.Event image={image} date={date} summary={summary} extraText={extraText} />
+            ))
+          }
+        </Feed>
       </Grid.Column>
       <Grid.Column width={5}>
         <Item.Group>
